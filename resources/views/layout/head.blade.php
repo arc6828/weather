a901496cf85b8763031ad63f6efc17850b4466b4
arc6828 <chavalit.kow@gmail.com>
2020-03-18 21:22:44 +0700

    Update head.blade.php

--- resources/views/layout/head.blade.php
+++ resources/views/layout/head.blade.php
@@ -7,7 +7,7 @@
 <!--link href="{{ asset ('css/bootstrap.min.css') }}" rel="stylesheet">
 <link href="{{ asset ('css/bootstrap-responsive.min.css') }}"  rel="stylesheet"-->
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-<link href="http://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,400,600" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,400,600" rel="stylesheet">
 <link href="{{ asset ('css/font-awesome.css') }}" rel="stylesheet">
 <link href="{{ asset ('css/style.css') }}" rel="stylesheet">
 <link href="{{ asset ('css/pages/dashboard.css') }}" rel="stylesheet" >
