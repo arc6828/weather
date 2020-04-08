5fca2681450aa628a4f362bff88db62b63803451
Chavalit Koweerawong <chavalit.kow@gmail.com>
2020-04-08 16:53:10 +0700

    Update WeatherController.php

--- app/Http/Controllers/WeatherController.php
+++ app/Http/Controllers/WeatherController.php
@@ -112,7 +112,7 @@ class WeatherController extends Controller
         //COPY MAIN OBJECT
         $small_object = json_decode(json_encode($weather));
         //MERGE DETAIL AND MAIN
-        $weather = (object) array_merge((array) $base_object, (array) $small_object, (array) $large_object ); 
+        $weather = (object) array_merge((array) $base_object, (array) $small_object, (array) $large_object['detail'] ); 
         
 
         return view('weather.show', compact('weather','base_object','small_object','large_object'));
