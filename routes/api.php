bab2906a854119c09a92e2f8081544025ada1c46
Chavalit Koweerawong <chavalit.kow@gmail.com>
2020-05-11 14:33:22 +0700

    change route

--- routes/api.php
+++ routes/api.php
@@ -19,8 +19,8 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::apiResource('weatherapi', 'API\WeatherController');
 
-Route::resource('ocr', 'API\OcrController');
-Route::resource('location', 'API\LocationController');
+Route::apiResource('ocr', 'API\OcrController');
+Route::apiResource('location', 'API\LocationController');
 Route::get('/map/locations','API\MapController@locations');
 Route::get('/map/staffgauges','API\MapController@staffgauges');
 Route::get('/map/ocrs','API\MapController@ocrs');
