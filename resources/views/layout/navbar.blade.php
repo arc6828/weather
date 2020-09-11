42cdf4aaa3b1480ce249a6a51256d9280447130b
arc6828 <chavalit.kow@gmail.com>
2020-09-11 18:59:02 +0700

    Update navbar.blade.php

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -1,8 +1,8 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-success" style="background: #00ba8b !important; ">
   <div class="container"> 
     <img class="mr-2 d-none" src="{{ url('/')}}/image/rmutsb_logo_en.png" height="50">
-    <img class="mr-2" src="{{ url('/')}}/image/logo_head.png" height="50">   
     <img class="mr-2" src="{{ url('/')}}/image/kaset.png" height="50">    
+    <img class="mr-2" src="{{ url('/')}}/image/logo_head.png" height="50">   
     <img class="mr-2 d-none" src="{{ url('/')}}/image/swg.png" height="50">
     <a class="navbar-brand" href="#">&nbsp;&nbsp;Real Time Flood Forecast System: Ramkhamhaeng Area</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
